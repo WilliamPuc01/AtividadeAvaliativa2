12b0af4d0c8ea21b2c5ed5eee2d8c244e1a6c3b3
WilliamPuc01 <william.kn@puccampinas.edu.br>
2024-05-13 19:21:11 -0300

    Primeiro commit

---
+++ teste1.py
@@ -0,0 +1,64 @@
+funcs={}
+func = []
+
+def insert():
+    matricula = int(input('Numero de Matricula: '))
+    nome = input('Nome p/ Cadastro: ')
+    cod_func = int(input('Código da função: '))
+    while(cod_func not in (101,102)):
+        cod_func = int(input('Código da função: '))
+    if(cod_func==101):
+        vvendas=float(input("Digite o volume de vendas: "))
+        renda=1500+vvendas*0.09
+    else:
+        renda = float(input('Salário bruto do funcionário: '))
+        while(renda<2150 or renda>6950):
+            renda = float(input('Salário bruto do funcionário: '))
+
+    imp=0
+    if(renda<=2259.2):
+        imp=0
+    elif(renda<=2828.65):
+        imp=0.075
+    elif(renda<=3751.05):
+        imp=0.15
+    elif(renda<=4664.68):
+        imp=0.225
+    else:
+        imp=0.275
+    sal = renda-renda*imp
+
+
+    pfalta = input('Esse funcionario faltou S/N: ')
+        
+    qtfalta = 0
+    falta = []
+    if pfalta.lower() == 's':
+        qtfalta = int(input('Quantas faltas: '))
+        renda = renda-(qtfalta*renda/30)
+        
+            
+    else: 
+        falta.append('Não possui falta')
+        qtFalta=0
+    func=[nome,cod_func,renda,qtfalta,sal,imp*100]
+    funcs[matricula]=func
+    print(funcs)
+def remove():
+    rmat=int(input("Digite a matrícula do funcionário que deseja remover: "))
+    del funcs [rmat]
+def imposto():
+
+
+
+
+
+ n = int(input('Digite a opção: '))
+
+while n>0:
+    if(n==1):
+        insert()
+        n = int(input('Digite a opção: '))
+    if(n==2):
+        remove()
+        n = int(input('Digite a opção: '))
